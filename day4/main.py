4b2b8b642b86a1fdd56434f38c6c54aaefe134fa
Darius Elphick <darius.elphick@trademe.co.nz>
2023-12-04 22:09:50 +1300

    Day 4 part 1

--- day4/main.py
+++ day4/main.py
@@ -8,78 +8,34 @@ def main():
   input = read_file(sys.argv[1])
   print("Input", input)
   
-  symbol_locations = get_symbol_locations(input)
-  #print_location_map(symbol_locations, "Symbol Locations")
-      
-  number_locations = get_number_locations(input)
-  #print_location_map(number_locations, "Number Locations")
+  score = 0
   
-  parts = get_parts(symbol_locations, number_locations)
-  print("Parts", parts)
+  for card in input:
+    score = score + get_card_score(card)
+    
+  print("Score", score)
   
-  counter = 0
-  for part in parts:
-    counter = counter + int(part)
-  print("Answer", counter)
+def get_card_score(card):
+  split_card = card.split("|")
+  winning_numbers = list(re.findall(r'\d+', split_card[0].split(":")[1]))
+  winning_numbers.sort()
+  actual_numbers = list(re.findall(r'\d+', split_card[1]))
+  actual_numbers.sort()
+  print("Winning", winning_numbers)
+  print("Actual", actual_numbers)
   
-def get_parts(symbol_locations, number_locations):
-  parts = []
-  for i, line in enumerate(symbol_locations):
-    for symbol in line:
-      
-      matches = []
-      
-      if i > 0:
-        print("Above line", symbol)
-        numbers_above = number_locations[i - 1]
-        for number in numbers_above:
-          print("Comparing symbol", symbol)
-          print("Comparing number", number)
-          if (symbol.start() == number.end()) or (symbol.end() == number.start()) or (symbol.start() >= number.start() and symbol.start() <= number.end()):
-            print("MATCH ABOVE!")
-            matches.append(number[0])
-            
-      print("Same line", symbol)
-      numbers_on_same_line = number_locations[i]
-      for number in numbers_on_same_line:
-        print("Comparing symbol", symbol)
-        print("Comparing number", number)
-        if (symbol.start() == number.end()) or (symbol.end() == number.start()):
-          print("MATCH ON LINE!")
-          matches.append(number[0])
-          
-      if i < len(symbol_locations) - 1:
-        print("Below line", symbol)
-        numbers_below = number_locations[i + 1]
-        for number in numbers_below:
-          print("Comparing symbol", symbol)
-          print("Comparing number", number)
-          if (symbol.start() == number.end()) or (symbol.end() == number.start()) or (symbol.start() >= number.start() and symbol.start() <= number.end()):
-            print("MATCH BELOW!")
-            matches.append(number[0])
-            
-      if len(matches) == 2 and symbol[0] == '*':
-        parts.append(functools.reduce(lambda a, b: int(a) * int(b), matches))
-        
-  return parts
-  
-def get_symbol_locations(input):
-  symbol_locations = []
-  for line in input:
-    symbol_locations.append(list(re.finditer(r'[^\d\n.]', line)))
-  return symbol_locations
-
-def get_number_locations(input):
-  number_locations = []
-  for line in input:
-    number_locations.append(list(re.finditer(r'\d+', line)))
-  return number_locations
-
-def print_location_map(location_map, label):
-  print(label)
-  for line_matches in location_map:
-    for match in line_matches:
-      print(match)
+  winning_count = 0
+  for number in actual_numbers:
+    try:
+      index = winning_numbers.index(number)
+      if winning_count == 0:
+        winning_count = winning_count + 1
+      else:
+        winning_count = winning_count * 2
+    except ValueError:
+      continue
+  print("Card score", winning_count)
+  return winning_count
   
 def read_file(filename):
   file = open(filename, 'r')
